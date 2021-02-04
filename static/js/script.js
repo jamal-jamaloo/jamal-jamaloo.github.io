7de5f4d1aaa44ea32b592d01579cfffdf2a699ff
jamal-jamaloo <78505242+jamal-jamaloo@users.noreply.github.com>
2021-02-04 18:49:03 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -27,8 +27,7 @@ $(document).ready(function () {
   //global variables
   action_name = "action_greet_user";
   sender_id = "jitesh97";
-  //myUrl = "https://a6cce702beb9.ngrok.io";    
-  myUrl = "localhost:5005";
+  myUrl = "https://a6cce702beb9.ngrok.io"; 
   //if you want the bot to start the conversation
 //   custom_action_trigger();
 });
