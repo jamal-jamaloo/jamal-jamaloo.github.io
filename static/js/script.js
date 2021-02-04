ec8e9042445a806f412a6d3915d7a991d6ba7c29
jamal-jamaloo <78505242+jamal-jamaloo@users.noreply.github.com>
2021-02-04 09:49:10 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -246,7 +246,7 @@ function setBotResponse(response) {
     hideBotTyping();
     if (response.length < 1) {
       //if there is no response from Rasa, send  fallback message to the user
-      var fallbackMsg = "I am facing some issues, please try again later!!!";
+      var fallbackMsg = "Jamal är en halvö mellan Obviken och Karahavet i nordvästra Sibirien i Ryssland. Jamal är 700 kilometer lång och 240 kilometer bred och är uppbyggd av sand och lera.";
 
       var BotResponse =
         '<img class="botAvatar" src="./static/img/sara_avatar.png"/><p class="botMsg">' +
