0f787fa2d91d43b67ac532e22c2668e01e15c067
jamal-jamaloo <78505242+jamal-jamaloo@users.noreply.github.com>
2021-04-07 21:15:34 +0200

    Update script.js

--- nacka/static/js/script.js
+++ nacka/static/js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
   //global variables
   action_name = "action_greet_user";
   sender_id = "jitesh97";
-  myUrl = "https://d63be13a4773.ngrok.io"; 
+  myUrl = "https://92a2bbb2ccad.ngrok.io"; 
   //if you want the bot to start the conversation
 //   custom_action_trigger();
 });
